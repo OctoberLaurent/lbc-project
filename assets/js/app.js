218f842933340ebe9870a39aa87b96b34a375d1f
Laurent Leplat <laurent@lepl.at>
2019-12-16 15:16:06 +0100

    'initial

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,10 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
